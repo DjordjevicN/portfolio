763a86cbdbe7ab1d1dbde6b6dd01add906d8dc7d
Nikola <nikola.dj.87@gmail.com>
2025-08-07 17:24:12 +0200

    more info and refactoring

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import Logo from "./Logo";
 const Footer = () => {
   return (
     <div className="min-h-[50vh] bg-black flex items-center justify-center">
-      <div className="text-white max-w-[1200px] mx-auto  w-full flex justify-center md:justify-between p-4 ">
+      <div className="text-white max-w-[1200px] mx-auto w-full flex  md:justify-between p-4 ">
         <div className="align-top hidden md:block">
           <Logo />
         </div>
-        <div className="flex-col sm:flex-row sm:flex  gap-16">
+        <div className="">
           <div className="flex flex-col gap-3">
             <h1 className="mb-8">Links</h1>
             <a
@@ -22,9 +22,6 @@ const Footer = () => {
             <a target="_blank" href="mailto:nikola.dj.87@gmail.com">
               nikola.dj.87@gmail.com
             </a>
-            {/* <a target="_blank" href="https://read.cv/nikola_djordjevic">
-              Read Resume
-            </a> */}
           </div>
         </div>
       </div>
